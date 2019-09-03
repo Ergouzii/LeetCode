690e2e69250fc0af52a7eb1957dd3c44904e4859
Xiaohui Liu <jayliu925@gmail.com>
2019-09-03 11:58:49 -0600

    greedy

---
+++ 455-AssignCookies.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        if not g or not s:
+            return 0
+        g.sort()
+        s.sort()
+        i = 0
+        j = 0
+        res = 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                res += 1
+                i += 1
+                j += 1
+            elif s[j] < g[i]:
+                j += 1
+        return res
